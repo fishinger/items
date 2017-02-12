848a29dd8459c554b168fe0f3ce117e9c5d5a350
fishinger <pk@iesa.ru>
2017-02-12 23:51:13 +0300

    edit

--- public/assets/js/url/url.js
+++ public/assets/js/url/url.js
@@ -1,3 +1,3 @@
 angular.module('app')
-.constant('baseUrl', 'http://localhost:5000/items/')
-.constant('usersUrl', 'http://localhost:5000/users/')
+.constant('baseUrl', 'http://92.53.119.39:5000/items/')
+.constant('usersUrl', 'http://92.53.119.39:5000/users/')
--- server.js
+++ server.js
@@ -25,61 +25,61 @@ dpd.on('error', function(err) {
   });
 });
 
-
-
-
-var mailOptions, transporter;
-var nodemailer = require('nodemailer');
-transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: 'pavel45888@gmail.com',
-    pass: '27041963elgoog'
-  }
-});
-
-
-
-//создаем коллекцию
-function Collection(url) {
-  this.url = url
-}
-
-Collection.prototype.request = function (options, fn) {
-  var url = this.url;
-  options.url = url + (options.url || '');
-  request(options, function (err, res, body) {
-    if(res.statusCode >= 400) {
-      err = body || {message: 'an unknown error occurred'};
-      return fn(err);
-    }
-
-    fn(null, body);
-  });
-}
-
-//подключаемся к колекции
-var c = new Collection('http://localhost:5000/items');
-c.request({url: '?done=false'}, function(err, todos) {
-  //console.log(todos); // [...]
-});
-
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:5000');
-
-//ловим событие регистрации и отправляем почту
-socket.on('reg', function (data) {
-  console.log(data.email); // emit()ed from the server
-  mailOptions = {
-    from: 'Pavel',
-    to: data.email,
-    subject: 'Hello',
-    html: '<b>test</b>'
-  };
-  transporter.sendMail(mailOptions, function(err, info) {
-    if (err) {
-      return console.log(err);
-    }
-    return console.log("Message sent: " + info.response);
-   });
-});
+//
+//
+//
+// var mailOptions, transporter;
+// var nodemailer = require('nodemailer');
+// transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     user: 'pavel45888@gmail.com',
+//     pass: '27041963elgoog'
+//   }
+// });
+//
+//
+//
+// //создаем коллекцию
+// function Collection(url) {
+//   this.url = url
+// }
+//
+// Collection.prototype.request = function (options, fn) {
+//   var url = this.url;
+//   options.url = url + (options.url || '');
+//   request(options, function (err, res, body) {
+//     if(res.statusCode >= 400) {
+//       err = body || {message: 'an unknown error occurred'};
+//       return fn(err);
+//     }
+//
+//     fn(null, body);
+//   });
+// }
+//
+// //подключаемся к колекции
+// var c = new Collection('http://localhost:5000/items');
+// c.request({url: '?done=false'}, function(err, todos) {
+//   //console.log(todos); // [...]
+// });
+//
+// var io = require('socket.io-client');
+// var socket = io.connect('http://localhost:5000');
+//
+// //ловим событие регистрации и отправляем почту
+// socket.on('reg', function (data) {
+//   console.log(data.email); // emit()ed from the server
+//   mailOptions = {
+//     from: 'Pavel',
+//     to: data.email,
+//     subject: 'Hello',
+//     html: '<b>test</b>'
+//   };
+//   transporter.sendMail(mailOptions, function(err, info) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     return console.log("Message sent: " + info.response);
+//    });
+// });
